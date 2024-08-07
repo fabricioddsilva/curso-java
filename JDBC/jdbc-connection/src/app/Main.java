95d66f40ccc6a93732328edb54e948771c85b0a7
Fabricio Dias <fabriciodsilva3404@gmail.com>
2024-08-07 09:01:32 -0300

    Conexão com banco de dados

---
+++ JDBC/jdbc-connection/src/app/Main.java
@@ -0,0 +1,12 @@
+package app;
+
+import db.DB;
+
+import java.sql.Connection;
+
+public class Main {
+    public static void main(String[] args) {
+        Connection conn = DB.getConnection();
+        DB.closeConnection();
+    }
+}
